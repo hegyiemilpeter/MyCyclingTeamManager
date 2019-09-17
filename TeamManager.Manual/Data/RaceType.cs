2612d0671e469e25225f859a9816e649d353401a
Emil Hegyi <hegyi.emil.peter@gmail.com>
2019-09-17 16:46:34 +0200

    Formatting of race type enums.

--- TeamManager.Manual/Data/RaceType.cs
+++ TeamManager.Manual/Data/RaceType.cs
@@ -1,17 +1,30 @@
-﻿namespace TeamManager.Manual.Data
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace TeamManager.Manual.Data
 {
     public enum RaceType
     {
+        [Display(Name = "Other")]
         Other,
+        [Display(Name = "Road race")]
         Road_RoadRace,
+        [Display(Name = "Individual time trial")]
         Road_TimeTrial,
+        [Display(Name = "MTB XCO")]
         MTB_CrossCountry,
+        [Display(Name = "MTB Marathon")]
         MTB_Marathon,
+        [Display(Name = "Full distance triathlon")]
         TRI_LongDistance,
+        [Display(Name = "Half distance triathlon")]
         TRI_HalfDistance,
+        [Display(Name = "Olimpic distance triathlon")]
         TRI_OlimpicDistance,
+        [Display(Name = "Sprint distance triathlon")]
         TRI_SprintDistance,
+        [Display(Name = "Running")]
         TRI_Running,
+        [Display(Name = "Duathlon")]
         TRI_Duathlon
     }
 }
---
+++ TeamManager.Manual/Models/EnumHelper.cs
@@ -0,0 +1,49 @@
+﻿using Microsoft.AspNetCore.Mvc.Rendering;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Reflection;
+
+public static class EnumHelper<T> 
+     where T : Enum
+{
+    public static IEnumerable<SelectListItem> GetSelectListItems()
+    {
+        IList<SelectListItem> selectListItems = new List<SelectListItem>();
+        Array values = typeof(T).GetEnumValues();
+        for (int i = 0; i < values.Length; i++)
+        {
+            selectListItems.Add(new SelectListItem(GetDisplayValue((T)values.GetValue(i)), values.GetValue(i).ToString()));
+        }
+
+        return selectListItems;
+    }
+
+    public static string GetDisplayValue(T value)
+    {
+        var fieldInfo = value.GetType().GetField(value.ToString());
+
+        var descriptionAttributes = fieldInfo.GetCustomAttributes(
+            typeof(DisplayAttribute), false) as DisplayAttribute[];
+
+        if (descriptionAttributes[0].ResourceType != null)
+            return lookupResource(descriptionAttributes[0].ResourceType, descriptionAttributes[0].Name);
+
+        if (descriptionAttributes == null) return string.Empty;
+            return (descriptionAttributes.Length > 0) ? descriptionAttributes[0].Name : value.ToString();
+    }
+
+    private static string lookupResource(Type resourceManagerProvider, string resourceKey)
+    {
+        foreach (PropertyInfo staticProperty in resourceManagerProvider.GetProperties(BindingFlags.Static | BindingFlags.NonPublic | BindingFlags.Public))
+        {
+            if (staticProperty.PropertyType == typeof(System.Resources.ResourceManager))
+            {
+                System.Resources.ResourceManager resourceManager = (System.Resources.ResourceManager)staticProperty.GetValue(null, null);
+                return resourceManager.GetString(resourceKey);
+            }
+        }
+
+        return resourceKey; // Fallback with the key name
+    }
+}
