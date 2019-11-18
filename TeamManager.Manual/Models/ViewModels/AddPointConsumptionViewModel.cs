340caf8edf0b37a92c3fe8117569402b362b8a3e
Emil Hegyi <hegyi.emil.peter@gmail.com>
2019-11-18 11:08:22 +0100

    [Localization] Points.

--- TeamManager.Manual/Models/ViewModels/AddPointConsumptionViewModel.cs
+++ TeamManager.Manual/Models/ViewModels/AddPointConsumptionViewModel.cs
@@ -1,5 +1,6 @@
 ﻿using Microsoft.AspNetCore.Mvc.ModelBinding;
 using Microsoft.AspNetCore.Mvc.Rendering;
+using Microsoft.Extensions.Localization;
 using System;
 using System.Collections.Generic;
 using System.ComponentModel.DataAnnotations;
@@ -9,22 +10,17 @@ namespace TeamManager.Manual.Models.ViewModels
     public class AddPointConsumptionViewModel
     {
         public IEnumerable<SelectListItem> Users { get; set; }
-        [Required]
+        [Required(ErrorMessage = "The user is required.")]
         public string SelectedUserId { get; set; }
         public string Remark { get; set; }
-        [Range(1, Double.MaxValue)]
+        [Range(1, Double.MaxValue, ErrorMessage = "Must be between 1 and 1000.")]
         public int Amount { get; set; }
 
         public void Validate(ModelStateDictionary modelState, int currentPointsOfUser)
         {
-            if(Amount <= 0)
-            {
-                modelState.AddModelError(nameof(Amount), "Amount cannot be 0.");
-            }
-
             if(Amount > currentPointsOfUser)
             {
-                modelState.AddModelError(nameof(Amount), $"The selected user does not have so many points. Maximum {currentPointsOfUser} is allowed.");
+                modelState.AddModelError(nameof(Amount), string.Format("The selected user does not have so many points. Maximum {0} is allowed.", currentPointsOfUser));
             }
         }
     }
