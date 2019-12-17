2c825ed2355eed14808bfa1e1967e080f5f8acce
Emil Hegyi <hegyi.emil.peter@gmail.com>
2019-12-17 11:17:31 +0100

    [Refactor] Added migration according to identifier refactor.

---
+++ TeamManager.Manual/Data/Migrations/20191217101632_RefactoredIdentifiersToUsers.cs
@@ -0,0 +1,90 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+namespace TeamManager.Manual.Data.Migrations
+{
+    public partial class RefactoredIdentifiersToUsers : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(
+                name: "IdentificationNumbers",
+                schema: "TeamManager");
+
+            migrationBuilder.AddColumn<string>(
+                name: "AkeszNumber",
+                schema: "TeamManager",
+                table: "AspNetUsers",
+                nullable: true);
+
+            migrationBuilder.AddColumn<string>(
+                name: "OtprobaNumber",
+                schema: "TeamManager",
+                table: "AspNetUsers",
+                nullable: true);
+
+            migrationBuilder.AddColumn<string>(
+                name: "TriathleteLicence",
+                schema: "TeamManager",
+                table: "AspNetUsers",
+                nullable: true);
+
+            migrationBuilder.AddColumn<string>(
+                name: "UCILicence",
+                schema: "TeamManager",
+                table: "AspNetUsers",
+                nullable: true);
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "AkeszNumber",
+                schema: "TeamManager",
+                table: "AspNetUsers");
+
+            migrationBuilder.DropColumn(
+                name: "OtprobaNumber",
+                schema: "TeamManager",
+                table: "AspNetUsers");
+
+            migrationBuilder.DropColumn(
+                name: "TriathleteLicence",
+                schema: "TeamManager",
+                table: "AspNetUsers");
+
+            migrationBuilder.DropColumn(
+                name: "UCILicence",
+                schema: "TeamManager",
+                table: "AspNetUsers");
+
+            migrationBuilder.CreateTable(
+                name: "IdentificationNumbers",
+                schema: "TeamManager",
+                columns: table => new
+                {
+                    Id = table.Column<int>(type: "int", nullable: false)
+                        .Annotation("SqlServer:Identity", "1, 1"),
+                    Type = table.Column<int>(type: "int", nullable: false),
+                    UserId = table.Column<int>(type: "int", nullable: false),
+                    Value = table.Column<string>(type: "nvarchar(max)", nullable: true)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_IdentificationNumbers", x => x.Id);
+                    table.ForeignKey(
+                        name: "FK_IdentificationNumbers_AspNetUsers_UserId",
+                        column: x => x.UserId,
+                        principalSchema: "TeamManager",
+                        principalTable: "AspNetUsers",
+                        principalColumn: "Id",
+                        onDelete: ReferentialAction.Cascade);
+                });
+
+            migrationBuilder.CreateIndex(
+                name: "IX_IdentificationNumbers_UserId",
+                schema: "TeamManager",
+                table: "IdentificationNumbers",
+                column: "UserId");
+        }
+    }
+}
